26e76ef40241bdf1fe5119d4041f5bd554131d18
abs1337 <salam@saberi.us>
2019-11-12 14:28:08 +0530

    Update toDoc.js

--- toDoc.js
+++ toDoc.js
@@ -1,7 +1,7 @@
 /**
  * Title : toDoc.Js - JavaScript Library to create create a Word document with Text/HTML
  * Author : Abs1337
- * Version : 2.1, 11/4/2019
+ * Version : 2.2, 11/4/2019
  * License : MIT
  * @namespace toDoc
  */
@@ -463,7 +463,7 @@
             var urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
             // Check for Data URL
             if (imageURL.indexOf("data") == 0) {
-                var imgHTML = "<div align='" + imageObj.imgAlign + "'><img width='" + imageObj.imgWidth + "' height='" + imageObj.imgHeight + "' src='" + imageURL + "' crossOrigin='anonymous'></img>";
+                var imgHTML = "<div align='" + imageObj.imgAlign + "'><img width='" + imageObj.imgWidth + "' height='" + imageObj.imgHeight + "' src='" + imageURL + "' crossOrigin='anonymous'></img></div>";
                 imageObj.oContent = imgHTML;
             } else if (urlRegex.test(imageURL)) { // Check for URL
                 var imgUrl = imageURL,
@@ -526,7 +526,7 @@
                     // Get canvas contents as a data URL
                     var imgAsDataURL = imgCanvas.toDataURL("image/png");
 
-                    var imgHTML = "<div align='" + imgAlign + "'><img width='" + imgWidth + "' height='" + imgHeight + "' src='" + imgAsDataURL + "' crossOrigin='anonymous'></img>";
+                    var imgHTML = "<div align='" + imgAlign + "'><img width='" + imgWidth + "' height='" + imgHeight + "' src='" + imgAsDataURL + "' crossOrigin='anonymous'></img></div>";
 
                     // Return data URL in HTML tags
                     if (callBack) {
@@ -552,7 +552,7 @@
                 }
                 var src = 'data:image/jpeg;base64,'+ window.btoa(binary);
 
-                var imgHTML = "<div align='" + imgAlign + "'><img width='" + imgWidth + "' height='" + imgHeight + "' src='" + src + "' crossOrigin='anonymous'></img>"
+                var imgHTML = "<div align='" + imgAlign + "'><img width='" + imgWidth + "' height='" + imgHeight + "' src='" + src + "' crossOrigin='anonymous'></img></div>"
 
                 // Return data URL in HTML tags
                 if (callBack) {
